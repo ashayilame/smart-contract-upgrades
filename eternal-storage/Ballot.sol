93ffb93d50365980c26d78992b112a3a61de32d5
ashayilame <34617757+ashayilame@users.noreply.github.com>
2022-05-25 11:55:23 +0530

    Create Ballot.sol

---
+++ eternal-storage/Ballot.sol
@@ -0,0 +1,27 @@
+library ballotLib {
+
+    function getNumberOfVotes(address _eternalStorage) public view returns (uint256)  {
+        return EternalStorage(_eternalStorage).getUIntValue(keccak256('votes'));
+    }
+
+    function setVoteCount(address _eternalStorage, uint _voteCount) public {
+        EternalStorage(_eternalStorage).setUIntValue(keccak256('votes'), _voteCount);
+    }
+}
+
+contract Ballot {
+    using ballotLib for address;
+    address eternalStorage;
+
+    constructor(address _eternalStorage) {
+        eternalStorage = _eternalStorage;
+    }
+
+    function getNumberOfVotes() public view returns(uint) {
+        return eternalStorage.getNumberOfVotes();
+    }
+
+    function vote() public {
+        eternalStorage.setVoteCount(eternalStorage.getNumberOfVotes() + 1);
+    }
+}
